1ad6cc83d527fa2f4663f4119adbad36d18b1f92
crobertson <crobertson@solidsys.net>
2023-07-25 13:44:04 -0700

    Non-breaking changes to macros and functions, filename can now be included to further specify settings files.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![warn(missing_docs)]
 
 use crate::LoadSettingsError::{DeserializationError, IOError};
+use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::fs::File;
 use std::io::{Error, Read, Write};
@@ -15,17 +16,59 @@ pub fn get_user_home() -> Option<PathBuf> {
 
 #[macro_export]
 /// Saves settings given a struct to save, to the home directory with a name matching the crate name
+/// ```
+/// use serde::{Deserialize, Serialize};
+/// use cr_program_settings::{load_settings, save_settings, save_settings_with_filename, load_settings_with_filename};
+///
+/// // create a struct we want to save, it needs to implement at a minimum of Serialize and Deserialize
+/// #[derive(Serialize,Deserialize, PartialEq, Debug)]
+/// struct Settings{
+/// setting1: u32,
+/// setting2: String,
+/// setting3: Vec<bool>,
+/// };
+///
+/// let settings = Settings{
+///     setting1: 128,
+///     setting2: "this is a cool setting struct".to_string(),
+///     setting3: vec![false,true,false,false]
+/// };
+///
+/// save_settings!(settings).expect("Settings were unable to be saved");
+///
+/// // -- snip --
+///
+/// let loaded_settings = load_settings!(Settings).expect("Unable to read settings file");
+///
+/// assert_eq!(settings,loaded_settings);
+///
+/// save_settings!(settings,"cool_filename.ser").expect("Unable to save settings with specific filename");
+///
+/// // -- snip --
+///
+/// let specific_settings_loaded = load_settings!(Settings,"cool_filename.ser").expect("Unable to load settings with specific filename");
+///
+/// assert_eq!(settings,specific_settings_loaded);
+///
+/// ```
 macro_rules! save_settings {
-    ($t:expr) => {
-        save_settings(env!("CARGO_CRATE_NAME"), &$t)
+    ($settings:expr) => {
+        save_settings(env!("CARGO_CRATE_NAME"), &$settings)
+    };
+    ($settings: expr, $file_name: expr) => {
+        save_settings_with_filename(env!("CARGO_CRATE_NAME"), &$file_name, &$settings)
     };
 }
 
 #[macro_export]
 /// Loads settings given a type to load, from the home directory with a name matching the crate name
+/// For usage examples, see save_settings!() documentation.
 macro_rules! load_settings {
-    ($t:ty) => {
-        load_settings::<$t>(env!("CARGO_CRATE_NAME"))
+    ($setting_type:ty) => {
+        load_settings::<$setting_type>(env!("CARGO_CRATE_NAME"))
+    };
+    ($setting_type:ty,$file_name: expr) => {
+        load_settings_with_filename::<$setting_type>(env!("CARGO_CRATE_NAME"), $file_name)
     };
 }
 
@@ -34,6 +77,9 @@ macro_rules! load_settings {
 macro_rules! delete_settings {
     () => {
         delete_settings(env!("CARGO_CRATE_NAME"))
+    };
+    ($file_name: expr) => {
+        delete_setting_file(env!("CARGO_CRATE_NAME"), $file_name)
     };
 }
 
@@ -48,10 +94,12 @@ pub enum SaveSettingsError {
     SerializationError(toml::ser::Error),
 }
 
-/// Saves the settings file given in a directory named using the crate name
-/// Given a struct and a crate name of "my_cool_rust_project", the program
-/// would save it to /home/username/my_cool_rust_project/my_cool_rust_project.ser
-pub fn save_settings<T>(crate_name: &str, settings: &T) -> Result<(), SaveSettingsError>
+/// Saves a serializable settings object to a given filename in USER_HOME/crate_name/file_name
+pub fn save_settings_with_filename<T>(
+    crate_name: &str,
+    file_name: &str,
+    settings: &T,
+) -> Result<(), SaveSettingsError>
 where
     T: Serialize,
 {
@@ -59,8 +107,7 @@ where
         None => Err(SaveSettingsError::FailedToGetUserHome),
         Some(home_dir) => {
             let settings_path = home_dir.join(PathBuf::from(crate_name));
-            let settings_file_path =
-                settings_path.join(PathBuf::from(format!("{}.ser", crate_name)));
+            let settings_file_path = settings_path.join(PathBuf::from(file_name));
             match fs::create_dir_all(&settings_path) {
                 Ok(_) => match File::create(settings_file_path) {
                     Ok(mut file) => match toml::to_string_pretty(&settings) {
@@ -78,6 +125,16 @@ where
     }
 }
 
+/// Saves the settings file given in a directory named using the crate name
+/// Given a struct and a crate name of "my_cool_rust_project", the program
+/// would save it to /home/username/my_cool_rust_project/my_cool_rust_project.ser
+pub fn save_settings<T>(crate_name: &str, settings: &T) -> Result<(), SaveSettingsError>
+where
+    T: Serialize,
+{
+    save_settings_with_filename(crate_name, format!("{}.ser", crate_name).as_str(), settings)
+}
+
 #[derive(Debug)]
 /// Enum state representing the possible errors that can occur when loading settings
 pub enum LoadSettingsError {
@@ -89,18 +146,19 @@ pub enum LoadSettingsError {
     DeserializationError(toml::de::Error),
 }
 
-/// Loads a given settings file from the home directory and the given crate name
-/// Given "my_cool_rust_project", the program would search in /home/username/my_cool_rust_project for a settings file
-pub fn load_settings<T>(crate_name: &str) -> Result<T, LoadSettingsError>
+/// Loads a settings serialized file from USER_HOME/crate_name/file_name
+pub fn load_settings_with_filename<T>(
+    crate_name: &str,
+    file_name: &str,
+) -> Result<T, LoadSettingsError>
 where
-    T: serde::de::DeserializeOwned,
+    T: DeserializeOwned,
 {
     match get_user_home() {
         None => Err(LoadSettingsError::FailedToGetUserHome),
         Some(home_dir) => {
             let settings_path = home_dir.join(PathBuf::from(crate_name));
-            let settings_file_path =
-                settings_path.join(PathBuf::from(format!("{}.ser", crate_name)));
+            let settings_file_path = settings_path.join(PathBuf::from(file_name));
             match File::open(settings_file_path) {
                 Ok(mut file) => {
                     let mut file_data = String::new();
@@ -118,10 +176,27 @@ where
     }
 }
 
+/// Loads a given settings file from the home directory and the given crate name
+/// Given "my_cool_rust_project", the program would search in /home/username/my_cool_rust_project for a settings file
+pub fn load_settings<T>(crate_name: &str) -> Result<T, LoadSettingsError>
+where
+    T: DeserializeOwned,
+{
+    load_settings_with_filename(crate_name, format!("{}.ser", crate_name).as_str())
+}
+
 /// Deletes the settings directory found in the <user home>/crate_name
 /// e.g. /home/username/my_cool_project
 pub fn delete_settings(crate_name: &str) -> io::Result<()> {
     let home_dir = get_user_home().unwrap();
     let settings_path = home_dir.join(PathBuf::from(crate_name));
     fs::remove_dir_all(settings_path)
+}
+
+/// Deletes a specific settings file
+pub fn delete_setting_file(crate_name: &str, file_name: &str) -> io::Result<()> {
+    let home_dir = get_user_home().unwrap();
+    let settings_path = home_dir.join(PathBuf::from(crate_name));
+    let settings_file = settings_path.join(file_name);
+    fs::remove_file(settings_file)
 }
--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -1,4 +1,4 @@
-use cr_program_settings::{delete_settings, load_settings, save_settings};
+use cr_program_settings::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
@@ -51,4 +51,31 @@ fn test_primary_macros() {
     assert_eq!(t, loaded_settings);
 
     delete_settings!().unwrap();
+}
+
+#[test]
+fn test_filename_macros() {
+    let s = TestStruct {
+        settings: Settings {
+            a: 4,
+            b: true,
+            c: "sa51das651sad65a1s65s1a65d".to_string(),
+            list: vec![
+                "1/e2'1]e.12][1.e1e1e1@!#!@#!%^%&^&(*()*()()_)_49898-8-*8-*/".to_string(),
+                "d65w4d163489wd41a68d4".to_string(),
+                "543468g494h4964e".to_string(),
+            ],
+        },
+        other_struct: Default::default(),
+    };
+
+    let file_name = "test_macro_settings";
+
+    let _ = save_settings!(&s, file_name).unwrap();
+
+    let loaded_settings = load_settings!(TestStruct, file_name).unwrap();
+
+    assert_eq!(loaded_settings, s);
+
+    delete_settings!(file_name).unwrap();
 }
